ffdf64683b90a0c526283612e1011b74b2d76091
Himani-chaudhary1996 <67844673+Himani-chaudhary1996@users.noreply.github.com>
2020-07-05 05:02:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
-import name from './app.json';
-import {withAppContextProvider} from './GlobalContext';
+import { withAppContextProvider } from './GlobalContext';
+import  name  from './app.json'
+AppRegistry.registerComponent('very-special', () => App);
 
-AppRegistry.registerComponent("very-special", () => App);
+if (Platform.OS === 'web') {
+    AppRegistry.runApplication(name, {
+        rootTag: document.getElementById('root'),
+    });
+}
